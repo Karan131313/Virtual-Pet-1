a0ae6adbe5e667e1a0bd227d23108b65cfe8ca3b
Karan131313 <72206082+Karan131313@users.noreply.github.com>
2020-12-22 17:32:00 +0530

    Add files via upload

---
+++ Virtual-Pet/sketch.js
@@ -0,0 +1,32 @@
+var Pet
+function setup(){
+    database = firebase.database();
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+    var ballPosition = database.ref("ball/position")
+    ballPosition.on("value", readPosition)
+}
+
+function Preload(){
+  this.loadImage = loadImage(dogImg.png);
+  this.loadImage = loadImage(dogImg1.png);
+}
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+
